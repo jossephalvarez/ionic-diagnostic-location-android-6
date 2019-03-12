97ae7f0b1ffe2f9403a4dc4c5eb14319008ba60c
Josseph Alvarez Villa <jossephalvarez@gmail.com>
2019-03-12 03:09:58 -0500

    Create diagnostic-component.ts

---
+++ diagnostic-component.ts
@@ -0,0 +1,89 @@
+  ionViewWillEnter() { 
+ if (this.platform.is('android')) {
+        //VERSION 1
+        //this.tryGeolocation();
+        //version 2
+        /*    this.subscription = this._gps.diagnosticGPS()
+              .flatMap(results => this._gps.checkGPS(results))
+              .flatMap((coords: Geoposition) => this.getOffersByLocation(coords))
+              .subscribe((offers: Offer[]) => {
+                this.processOffers(offers);
+                this.loading.dismiss().then(r => {
+                  this.showText = true;
+                });
+              }, error => {
+                this.loading.dismissAll();
+                console.log("ERROR----------->" + JSON.stringify(error));
+                this.presentToast('Ups, ha ocurrido un error.Inténtalo en breve.');
+              })*/
+
+        //version 3
+        let permission = this.diagnostic.permission;
+        let permissionStatus = this.diagnostic.permissionStatus;
+        this.diagnostic.requestRuntimePermission(permission.ACCESS_FINE_LOCATION).then(statuses => {
+          for (var permission in statuses) {
+            switch (statuses[permission]) {
+              case permissionStatus.GRANTED:
+                alert("Permission granted to use " + permission);
+                this.getLocation();
+                break;
+              case permissionStatus.NOT_REQUESTED:
+                alert("Permission to use " + permission + " has not been requested yet");
+                break;
+              case permissionStatus.DENIED:
+                alert("Permission denied to use " + permission + " - ask again?");
+                break;
+              case permissionStatus.DENIED_ALWAYS:
+                alert("Permission permanently denied to use " + permission + " - guess we won't be using it then!");
+                break;
+            }
+          }
+        })
+          .catch(error => alert("ERROR" + JSON.stringify(error)))
+      }
+      
+      }
+      
+      getLocation() {   
+    this.diagnostic.isLocationEnabled().then(enabled => {     
+      if (enabled) {       
+        let options = {enableHighAccuracy: true}
+        this.geolocation.getCurrentPosition(options)
+          .then(res => {
+            alert(JSON.stringify(res))
+          }, error => alert("ERROR" + JSON.stringify(error)))
+          .catch(error => alert("CACTH" + error))
+      } else {
+        this.activateGPS()
+      }
+    })
+  }
+  activateGPS() {
+    let title, message;
+
+    title = `Activa tu GPS`;
+    message = `Necesitamos que actives tu GPS para ver la siguiente pantalla`;
+
+
+    let confirm = this.alertCtrl.create({
+      title: title,
+      message: message,
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {
+            this.navCtrl.pop();
+          }
+        },
+        {
+          text: 'Aceptar',
+          handler: () => {
+            this.navCtrl.pop().then(() => this.diagnostic.switchToLocationSettings())
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+  
+  }
